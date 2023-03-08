7cc988a7f2df2009c0040863605f9f0e5098a32a
Spencer Au <spencerau96@gmail.com>
2023-03-08 12:05:39 -0800

    .

---
+++ In Class Python Example/test.py
@@ -0,0 +1,135 @@
+#import library
+import sqlite3
+
+print()
+#print('import')
+#add path to chinook.db to connect to it
+connection = sqlite3.connect("chinook.db")
+#print('Connect to Chinook.db')
+
+#cursor object that executes SQL commands
+cur_obj = connection.cursor()
+#print('init cursor')
+#print()
+
+
+def createTable():
+    # query to create a table called tweet
+    new_table = '''
+    CREATE TABLE tweet(
+        tweetID INTEGER NOT NULL PRIMARY KEY,
+        Text VARCHAR(280),
+        creationDate DATETIME,
+        User VARCHAR(20),
+        Likes INTEGER,
+        Retweets INTEGER,
+        Comments INTEGER
+    );
+    '''
+    # execute query to create table
+    cur_obj.execute(new_table)
+    # commit modification to database
+    connection.commit
+    print("Table has been created")
+
+
+def insertHCQuery():
+    # query to insert a hardcoded value into tweet
+    query = '''
+    INSERT INTO tweet VALUES(1, 'This is a Tweet', '2023-01-01', '@Bob', 1,2,3)
+    '''
+    cur_obj.execute(query)
+    connection.commit()
+    print("Tweet has been inserted into table")
+
+
+def insertQ_Query():
+    record = (2, 'This is not a tweet', '2023-01-02', '@Alice')
+    query = '''
+    INSERT INTO tweet(tweetID, Text, creationDate, User)
+    VALUES(?,?,?,?)
+    '''
+    cur_obj.execute(query, record)
+    connection.commit()
+    print("QTweet has been inserted into table")
+
+
+def insertExecuteMany():
+    records = [
+        (3, 'Hello World', '2023-01-03', '@Eve'),
+        (4, 'Hello Universe', '2023-01-04', '@Bob'),
+        (5, 'This is Patrick', '2023-01-05', '@pStar')
+    ]
+    query = '''
+    INSERT INTO tweet(tweetID, Text, creationDate, User)
+    VALUES(?,?,?,?)
+    '''
+    # execute many objects
+    cur_obj.executemany(query, records)
+    connection.commit()
+    print("Multiple tweets have been inserted")
+
+
+def updateExecuteMany():
+    newData = [
+        # 4 likes, 5 comments, 10 comments, for id 2
+        (4, 5, 10, 2),
+        (10, 15, 20, 3),
+        (500000, 0, 1, 5)
+    ]
+    query = '''
+    UPDATE tweet
+    SET Likes = ?, Comments = ?, Retweets = ?
+    WHERE tweetID = ?
+    '''
+    cur_obj.executemany(query, newData)
+    connection.commit()
+    print("Tweets have been updated")
+
+
+def selectTweets():
+    query = '''
+    SELECT *
+    FROM tweet;
+    '''
+    # result is a cursor object
+    result = cur_obj.execute(query)
+    #connection.commit()
+
+    for row in result:
+        print(row)
+
+
+def selectPlaceholder():
+    # name to fetch result for
+    name = '@pStar'
+
+    query = '''
+    SELECT *
+    FROM tweet
+    WHERE User = '%s'
+    '''
+    result = cur_obj.execute(query % name)
+    for row in result:
+        print(row)
+
+
+def selectNamedPlaceholder():
+    # name and id to fetch
+    name = '@Bob'
+    id = 1
+
+    # using named placeholders, search tweet
+    query = '''
+    SELECT *
+    FROM tweet
+    WHERE User =:username
+    AND tweetID =:tID
+    '''
+
+    # pass parameters as a dictionary
+    # in {name1:var}, where it finds name1 in query, replace with var
+    result = cur_obj.execute(query, {'username':name, 'tID':id})
+    for row in result:
+        print(row)
+    
--- PA3/pa3.sql
+++ PA3/pa3.sql
@@ -21,35 +21,69 @@ SELECT MediaTypeId,
        GenreId,
        COUNT(*) AS Total,
        MAX(UnitPrice) AS MaxUnitPrice
-FROM tracks
-GROUP BY tracks.MediaTypeId, tracks.GenreId;
+FROM tracks AS t
+GROUP BY t.MediaTypeId, t.GenreId;
 
 /* 4) Write a query to get the average duration of tracks (in milliseconds) for each genre. Must have
 genre name in the result. */
-SELECT g.Name,
+SELECT g.Name AS Genre,
        AVG(Milliseconds) AS AvgDuration
 FROM tracks AS t
 INNER JOIN genres AS g
 ON t.GenreId = g.GenreId
 GROUP BY t.GenreId;
 
-
 /* 5) Write a query to show the total number of albums per artist name. */
+SELECT Name AS Artist,
+       COUNT(*) AS NumAlbums
+FROM artists
+NATURAL JOIN albums
+GROUP BY ArtistId;
 
 /* 6) Write a query to return the total number of invoices per billing city, billed in USA. */
+SELECT BillingCity,
+       COUNT(*) AS NumInvoices
+FROM invoices
+WHERE BillingCountry = 'USA'
+GROUP BY BillingCity;
 
 /* SET 2 */
 
 /* 1) Write a query to return the average duration of tracks (in milliseconds) for each composer, for tracks with a
    duration shorter than 375000 milliseconds. */
+SELECT Composer,
+       AVG(Milliseconds) AS AvgDuration
+FROM tracks
+WHERE milliseconds < 375000
+GROUP BY Composer;
 
 /* 2) Write a query to return the average duration of tracks (in milliseconds) for each composer, where the average
    duration is less than 375000 milliseconds. */
+SELECT Composer,
+       AVG(Milliseconds) AS AvgDuration
+FROM tracks
+GROUP BY Composer
+HAVING AvgDuration < 375000;
 
 /* 3) Find the names of all billing countries that have fewer than 10 records. */
-
+SELECT BillingCountry
+FROM invoices
+GROUP BY BillingCountry
+HAVING COUNT(*) < 10;
 
 /* 4) Find the name of the billing country that has 8 cities in the invoices table. */
+SELECT BillingCountry
+FROM invoices
+GROUP BY BillingCountry
+HAVING COUNT(DISTINCT BillingCity) = 8;
 
 /* 5) Write a query to find billing countries and the sum of their totals, that have more than 5 records
 each, from the year 2010. */
+SELECT BillingCountry,
+       SUM(Total)
+FROM invoices
+WHERE InvoiceDate LIKE '%2010%'
+GROUP BY BillingCountry
+HAVING COUNT(*) > 5;
+
+/* SET 3 Convert 1.1, 1.2, 1.4, 2.2, 2.5 to Relational Algebra. */
